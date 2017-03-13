aa0c1321dc65e44947559359445f5da946229d82
Damian Bulak <dbulak@mion.elka.pw.edu.pl>
2017-03-13 20:27:32 +0100

    Initial Spring Boot project.

---
+++ src/main/java/com/pik01/pharmaciesmanager/PharmaciesManagerApplication.java
@@ -0,0 +1,12 @@
+package com.pik01.pharmaciesmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PharmaciesManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PharmaciesManagerApplication.class, args);
+	}
+}
